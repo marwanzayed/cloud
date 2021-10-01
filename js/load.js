1c3081db03d507b718a16a0516d8ae45d36499c8
Marwan <ma12312034@gmail.com>
2021-10-01 20:06:41 +0200

    Update File

---
+++ js/load.js
@@ -0,0 +1,8 @@
+let loadingScreen = document.querySelector(".loader");
+
+window.addEventListener("load", setTimeout(hideLoading, 2000));
+
+function hideLoading() {
+  loadingScreen.style.cssText = "opacity: 0; transition: all 1s ease-in-out;";
+  setTimeout(() => (loadingScreen.style.cssText = "display: none;"), 1000);
+}
